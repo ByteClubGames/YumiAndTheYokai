03973991f229f1ce8ab9d24752148c487b936967
Unknown <theredpok@yahoo.com>
2018-03-10 11:56:28 -0800

    Added very basic pause menu
    This pause menu has a resume button and a menu button that does not do anything.
    It only works with keyboard and mouse input at the moment.

---
+++ CompSciClubSpring2018/Assets/Scripts/Pause Menu/PauseMenuScript.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Programmer: Darrell Wong
+ * Start Date: 3/10/2018
+ * Last Updated: 
+ * Script for the PauseMenu
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+
+    public bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Menu()
+    {
+        
+    }
+}
