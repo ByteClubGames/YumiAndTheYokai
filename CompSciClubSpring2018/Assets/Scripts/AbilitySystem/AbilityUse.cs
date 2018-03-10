86568e21b12feba854387008ed13c4677f827853
Jack Bruce <jackbruce0@gmail.com>
2018-03-10 13:32:13 -0800

    Projectile testing scene.
    Press black button and projectile flies!

---
+++ CompSciClubSpring2018/Assets/Scripts/AbilitySystem/AbilityUse.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AbilityUse : MonoBehaviour {
+
+    public GameObject fireballPrefab;
+    private FireBall fba;
+    private Stopwatch abilityCooldownTimer;
+    private Button button;
+    private Image fillImage;
+
+    public void OnAbilityUse(GameObject btn)
+    {
+        //if ability is not on cool down use it
+        fillImage = btn.transform.GetChild(0).gameObject.GetComponent<Image>();
+        UnityEngine.Debug.Log(btn.transform.GetChild(0).gameObject.name);
+        button = btn.GetComponent<Button>();
+        button.interactable = false;
+        fillImage.fillAmount = 1;
+        abilityCooldownTimer = new Stopwatch();
+        abilityCooldownTimer.Start();
+        fba = new FireBall();
+
+        GameObject go = Instantiate<GameObject>(fireballPrefab);
+
+        StartCoroutine(SpinImage());
+
+    }
+
+    private IEnumerator SpinImage() 
+    {
+        UnityEngine.Debug.Log(fba.AbilityCoolDown);
+        while (abilityCooldownTimer.IsRunning && abilityCooldownTimer.Elapsed.TotalSeconds < fba.AbilityCoolDown)
+        {
+            UnityEngine.Debug.Log(fillImage.fillAmount);
+            fillImage.fillAmount = ((float)abilityCooldownTimer.Elapsed.TotalSeconds / fba.AbilityCoolDown);
+            yield return null;
+        }
+        fillImage.fillAmount = 0;
+        button.interactable = true;
+        abilityCooldownTimer.Stop();
+        abilityCooldownTimer.Reset();
+
+        yield return null;
+    }
+
+}
