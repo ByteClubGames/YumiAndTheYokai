4809258e476384d07f6cb84a06b6cd8e513ff7f1
Persomatey <huntergoodin1@hotmail.com>
2018-04-06 11:36:18 -0700

    Made so much better
    Read above

---
+++ CompSciClubSpring2018/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("HunterGoodin-FirstScene");
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
