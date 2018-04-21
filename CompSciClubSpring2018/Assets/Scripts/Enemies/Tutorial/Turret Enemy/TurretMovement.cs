f84c6c9045acdf24820798badcae8510d7c6293a
Keiran Glynn <jamesglynn.k@gmail.com>
2018-04-21 12:21:06 -0700

    Enemy Will Walk on Ceilings and Floors
    The enemy will walk on ceilings and floors provided his rotation is either 0 or 180.

---
+++ CompSciClubSpring2018/Assets/Scripts/Enemies/Tutorial/Turret Enemy/TurretMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretMovement : MonoBehaviour {
+    private Vector2 turretPos;
+    private Transform turretRB;
+    private float minRange = 0f;
+    private float maxRange = 0f;
+    private int direction = -1;
+
+    public float patrolRange = 5f;
+    public float speed = 2f;
+
+
+	// Use this for initialization
+	void Start () {
+		turretRB = GameObject.Find("TurretEnemy").GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        OnPatrol(patrolRange);
+	}
+
+    private void OnPatrol(float range)
+    {
+        Debug.Log("I should be moving");
+        turretPos = GameObject.Find("TurretEnemy").GetComponent<Transform>().position;
+        range = patrolRange;
+        minRange = -1 * range;
+        maxRange = range;
+
+        switch (direction)
+        {
+            case -1:
+                if(turretPos.x > minRange)
+                {
+                    turretRB.transform.Translate(-transform.right * Time.deltaTime * speed);
+                }
+                else
+                {
+                    direction = 1;
+                }
+                break;
+            case 1:
+                if (turretPos.x < maxRange)
+                {
+                    turretRB.transform.Translate(transform.right * Time.deltaTime * speed);
+                }
+                else
+                {
+                    direction = -1;
+                }
+                break;
+        }
+    }
+}
