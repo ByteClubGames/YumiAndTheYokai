d843c7a8a28249cb7dc0f613d9c4d5a5afbfe5f2
Jack Bruce <jackbruce0@gmail.com>
2018-04-29 07:18:01 -0700

    Buttonish
    chiku

---
+++ CompSciClubSpring2018/Assets/Scripts/Abilities/IceSpell/IceSpellButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class IceSpellButton : MonoBehaviour {
+
+    public Button butt;
+    public GameObject spawner;
+    private bool spellOn;
+    private int objCount;
+
+	private void Start()
+	{
+        Button btn = butt.GetComponent<Button>();
+        spellOn = false;
+        btn.onClick.AddListener(TaskOnClick);
+	}
+
+    void TaskOnClick()
+    {
+        Debug.Log("You clicked the shit out of this button...");
+
+        if (!spellOn) {
+            Instantiate(spawner)
+            spellOn = true;
+        } else {
+            DestroyImmediate(spawner, true);
+            spellOn = false;
+        }
+    }
+}
