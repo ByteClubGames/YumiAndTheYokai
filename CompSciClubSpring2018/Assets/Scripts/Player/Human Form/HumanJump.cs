ef8406fa3ef431c7da7de25df412ff046833fbb5
Daniel Jaffe <flarros@gmail.com>
2018-05-07 02:11:35 -0700

    Had to revert jump script
    Whoever changed the Human jump script-- Hunter's jump scrip has a few bugs at this time so please leave the version I put on there. If you do change any script, please test it in the demo level before you push and also note the change when you push. Thanks!

--- CompSciClubSpring2018/Assets/Scripts/Player/Human Form/HumanJump.cs
+++ CompSciClubSpring2018/Assets/Scripts/Player/Human Form/HumanJump.cs
@@ -1,44 +1,81 @@
 ﻿/*
- * Programmer:   Hunter Goodin 
+ * Programmer:   Hunter Goodin  --- MODIFIED by Dan Jaffe
  * Date Created: 02/16/2018 @  9:40 PM 
- * Last Updated: 02/16/2018 @ 11:35 PM 
+ * Last Updated: 02/16/2018 @ 11:35 PM --- MODIFIED 5/7/18 @ 2:05 AM
  * File Name:    PlayerJumper.cs 
- * Description:  This script will be responsible for the player's movements. 
+ * Description:  This script will be responsible for the human jump. 
  */
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class HumanJump : MonoBehaviour
 {
-    public Rigidbody2D humanRB;
+
+    public Rigidbody2D HumanRB;
     public float jumpSpeed;
+    public bool isGrounded;
 
-    public Vector2 firstPressPos = new Vector2(0, 0);
-    public Vector2 secondPressPos;
-    public Vector2 currentSwipe;
-
-    private bool isGrounded; // Boolean variable than represents whether or not an object is grounded or not.
+    void Start()
+    {
+        HumanRB = GetComponent<Rigidbody2D>();
+    }
 
     private void FixedUpdate()
     {
         Jump();
-        //Swiper();
     }
 
-    private void Jump() // Script that allows the player to jump.
+    private void Jump()
     {
-            if (Input.GetKeyDown("space") && (humanRB.velocity.y == 0f) && isGrounded)
-            {
-                humanRB.AddForce(Vector2.up * Time.deltaTime * jumpSpeed);
-            }
+        if (Input.GetKey("space") && (HumanRB.velocity.y == 0f))
+        {
+            HumanRB.AddForce(Vector2.up * Time.deltaTime * jumpSpeed);
+
+        }
     }
 
     public void SetIsGrounded(bool value) // Allows the player to set isGrounded.
     {
         isGrounded = value;
     }
+}
+
+
+
+///********** DO NOT USE THE CODE BELOW-- IT IS BROKEN AND CAUSES SIGNIFICANT PROBLEMS WITH JUMP*********////
+
+
+
+//public Rigidbody2D humanRB;
+//public float jumpSpeed;
+
+//public Vector2 firstPressPos = new Vector2(0, 0);
+//public Vector2 secondPressPos;
+//public Vector2 currentSwipe;
+
+//private bool isGrounded; // Boolean variable than represents whether or not an object is grounded or not.
+
+//private void FixedUpdate()
+//{
+//    Jump();
+//    //Swiper();
+//}
+
+//private void Jump() // Script that allows the player to jump.
+//{
+//    if (Input.GetKeyDown("space") && (humanRB.velocity.y == 0f) && isGrounded)
+//    {
+//        humanRB.AddForce(Vector2.up * Time.deltaTime * jumpSpeed);
+//    }
+//}
+
+//public void SetIsGrounded(bool value) // Allows the player to set isGrounded.
+//{
+//    isGrounded = value;
+//}
 
     //private void Swiper()
     //{
@@ -65,4 +102,4 @@ public class HumanJump : MonoBehaviour
     //        currentSwipe = new Vector2(0, 0);
     //    }
     //}
-}
+//}
