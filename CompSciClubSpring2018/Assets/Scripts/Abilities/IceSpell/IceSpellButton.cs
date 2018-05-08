5faa31fa6accdc785cab67a130e40f1acec1b615
Jack Bruce <jackbruce0@gmail.com>
2018-05-08 15:58:38 -0700

    Feel the breeze!
    WindSpell prefab created
    WindSpellMover.cs moves prefab to target locations 
    -Challenge will be having our WindSpellSpawner object populate the target array upon dragging a finger

--- CompSciClubSpring2018/Assets/Scripts/Abilities/IceSpell/IceSpellButton.cs
+++ CompSciClubSpring2018/Assets/Scripts/Abilities/IceSpell/IceSpellButton.cs
@@ -1,4 +1,14 @@
-﻿using System.Collections;
+﻿/* IceSpellButton.cs
+ * Date Created: 4/24/18
+ * Last Edited: 5/08/18
+ * Programmer: Jack Bruce
+ * Description: Turns spell on and off.
+ *  - 1st press spawns spell spawning obj
+ *  - 2nd press destroys that object
+ *  This can very easily be implemented to other spell buttons by changing the
+ *  'spawner' variable to a prefab of your choosing.
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
---
+++ CompSciClubSpring2018/Assets/WindSpellMover.cs
@@ -0,0 +1,38 @@
+﻿/* WindSpellMover.cs
+ * Date Created: 5/08/18
+ * Last Edited: 5/08/18
+ * Programmer: Jack Bruce
+ * Description: Moves 'WindSpell' obj on desired path
+ *  - WindSpellSpawner will populate target array
+ *  - once activated WindSpell will move to each position in array
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindSpellMover : MonoBehaviour {
+
+	public Transform[] target;
+	public float speed;
+
+	private int current;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// move until you reach the current obj
+		if (transform.position != target[current].position)
+		{
+			Vector3 pos = Vector3.MoveTowards(transform.position,
+											  target[current].position, speed * Time.deltaTime);
+			GetComponent<Rigidbody>().MovePosition(pos);
+		}
+		else current = (current + 1) % target.Length; // obj reached, move to the next obj
+	}
+}
