c743975ca6089afd3008706c691ec1bb63d86487
Jack Bruce <jackbruce0@gmail.com>
2018-05-10 23:50:20 -0700

    Progress?
    Okay... So WindSpellUse.cs populates targets[] with transform objects upon clicks. Once enough targets[] is full, WindSpellPrefab is spawned and it is supposed to be passed targets[] from WindSpellUse.cs. Than that script will send the ice spell on that path.
    Problem #1: Way too many transform objects are spawned at once. The spawner needs to wait for an interval of time before spawning another transform
    Problem #2: I can't figure out how to pass the populated targets array from out instance of the WindSpellSpawner. I can access the script but I can't get info about the specific instance of that script.

--- CompSciClubSpring2018/Assets/Scripts/Abilities/WindSpell/WindSpellMover.cs
+++ CompSciClubSpring2018/Assets/Scripts/Abilities/WindSpell/WindSpellMover.cs
@@ -1,10 +1,11 @@
 ﻿/* WindSpellMover.cs
  * Date Created: 5/08/18
- * Last Edited: 5/08/18
+ * Last Edited: 5/10/18
  * Programmer: Jack Bruce
  * Description: Moves 'WindSpell' obj on desired path
- *  - WindSpellSpawner will populate target array
- *  - once activated WindSpell will move to each position in array
+ *  - WindSpellSpawner will populate targetarray
+ *  - once activated WindSpellUse will pass the target array to WindSpellMover (This is the current issue)
+ *  - then WindSpell obj will follow path declared by array
  */
 
 using System.Collections;
@@ -13,26 +14,31 @@ using UnityEngine;
 
 public class WindSpellMover : MonoBehaviour {
 
-	public Transform[] target;
-	public float speed;
+	public float speed = 10;
 
+	private Transform[] targets = new Transform[100];
+	private Transform nextPos;
 	private int current;
 
 	// Use this for initialization
 	void Start () {
-		
+		WindSpellUse spellUse = new WindSpellUse();
+		targets = spellUse.GetTransforms(); //this does not get the loaded array.. it is an empty array
+		current = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		nextPos = targets[current];
 		// move until you reach the current obj
-		if (transform.position != target[current].position)
+		if (transform.position != nextPos.position)
 		{
 			Vector3 pos = Vector3.MoveTowards(transform.position,
-											  target[current].position, speed * Time.deltaTime);
+			                                  nextPos.position, speed * Time.deltaTime);
 			GetComponent<Rigidbody>().MovePosition(pos);
 		}
-		else current = (current + 1) % target.Length; // obj reached, move to the next obj
+		else current = (current + 1) % targets.Length; // obj reached, move to the next obj
 	}
+
+
 }
--- CompSciClubSpring2018/Assets/Scripts/Abilities/WindSpell/WindSpellUse.cs
+++ CompSciClubSpring2018/Assets/Scripts/Abilities/WindSpell/WindSpellUse.cs
@@ -1,50 +1,81 @@
-﻿/* IceSpellUse.cs
- * Date Created: 3/10/18
- * Last Edited: 3/10/18
- * Programmer: Jack Bruce
- * Description: Copied from 'IceSpellUse.cs'... this needs work
- * Attatch to WindSpellSpawner
+﻿/* WindSpellUse.cs
+ * Date Created: 4/06/18
+ * Last Edited: 4/10/18
+ * Programmer: Jack Bruce && Stephen
+ * Description: Modified from 'IceSpellUse.cs'
+ * Attatch to WindSpellSpawner (This script is Active during "Draw Mode")
  *  -should spawn target objs (should be type 'Transform' instead of GameObject)
  *   upon dragging
- *  -should add target objs to target array in 'WindSpellUse'
+ *  -makes array of Target object (Empty game objects just used for Transform)
  */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WindSpellUse : MonoBehaviour {
-    
-	public GameObject targetPrefab;
-    private GameObject myCurrentObject;
+public class WindSpellUse : MonoBehaviour
+{
 
-    private bool _isDragging = false;
+	public GameObject targetPrefab; //empty gameobject used for Transform
+	public GameObject player;
+	public GameObject windSpellPrefab;
+	public int trgtAmnt;
+
+	private GameObject windSpell;
+	private Transform[] targets;
+	private int trgtNX;
+	private bool drawMode;
+
+	private bool _isDragging = false;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		drawMode = true;
+		trgtNX = 0;
+		targets = new Transform[trgtAmnt];
+		targets[trgtNX++] = player.transform;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
+
+		//Once array is full OR Draw mode is manually turned off
+		//Spawn WindSpell Object and load it with target array
+		if (trgtNX >= targets.Length && drawMode)
+		{
+			drawMode = false;
+			windSpell = Instantiate(windSpellPrefab, targets[0]); //spawn wind spell object @ player pos
+		}
 
 		if (Input.GetMouseButtonDown(0))
-        {
-            _isDragging = true;
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            _isDragging = false;
-            return;
-        }
-        if (_isDragging)
-        {
-            //Runs ice prefab is mouse button is pushed down
-            Vector3 p = Camera.main.ScreenToWorldPoint(new
-              Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
-			Instantiate(targetPrefab, new Vector3(p.x, p.y, 0.0f),
-                        Quaternion.identity);
-            targetPrefab.transform.position = Input.mousePosition;
-        }
+		{
+			_isDragging = true;
+		}
+		if (Input.GetMouseButtonUp(0))
+		{
+			_isDragging = false;
+			return;
+		}
+		if (_isDragging && drawMode) //SPAWNS WAY TO MANY OBJECTS AT ONCE! try per time
+		{
+			//Runs wind prefab is mouse button is pushed down
+			Vector3 p = Camera.main.ScreenToWorldPoint(new
+			Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+			Instantiate(targetPrefab, new Vector3(p.x, p.y, 0.0f), //spawns target
+						Quaternion.identity);
+
+			targetPrefab.transform.position = Input.mousePosition; //@ mouse position
+			targets[trgtNX++] = targetPrefab.transform; //Adds position to target array
+
+
+		}
 
 	}
+
+	public Transform[] GetTransforms() //called in WindSpellMover.cs to set positions
+	{
+		return targets;
+	}
+
 }
