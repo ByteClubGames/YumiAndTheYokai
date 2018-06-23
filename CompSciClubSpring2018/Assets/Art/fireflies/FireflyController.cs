8b914405b77593dafc604b9b1178099b67448a09
Daniel Jaffe <flarros@gmail.com>
2018-06-23 02:35:42 -0700

    Merge art-assets-upload
    Grabbed all of the art assets from that branch and stuck them in the art folder of master. Again, due to meta files, it was easier to just grab the stuff

---
+++ CompSciClubSpring2018/Assets/Art/fireflies/FireflyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireflyController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localPosition += transform.forward * Time.deltaTime * 2f;
+        this.transform.localPosition += transform.up * .1f * Mathf.Sin(Time.fixedTime * 3.0f * Random.Range(0.1f,0.5f));
+    }
+}
