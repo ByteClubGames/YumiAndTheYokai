fd5b655bb73c8280d6ac9d74c44f26c58adedddb
Brenden Plong <Karunaplong@yahoo.com>
2018-07-25 02:53:41 -0700

    Moving platform toggle Experiment
    Added moving toggle script and experimenting inside Moving platform scene

---
+++ CompSciClubSpring2018/Assets/Scripts/Enviornment/MovingPlatformToggle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformToggle : MonoBehaviour {
+    public Transform platform;
+    public Transform position1;
+    public Transform position2;
+    public Vector3 newPosition;
+    public string currentState;
+    public float smooth;
+    public float resetTime;
+	// Use this for initialization
+	void Start () {
+        ChangeTarget();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        platform.position = Vector3.Lerp(platform.position, newPosition, smooth * Time.deltaTime);
+	}
+    void ChangeTarget()
+    {
+        if(currentState == "Moving to position 1")
+        {
+            currentState = "Moving to position 2";
+            newPosition = position2.position;
+        }
+        else if (currentState == "Moving to position 2")
+        {
+            currentState = "Moving to position 1";
+            newPosition = position1.position;
+        }
+        else if (currentState == "")
+        {
+            currentState = "Moving to position 2";
+            newPosition = position2.position;
+        }
+        Invoke("ChangeTarget", resetTime);
+    }
+}
