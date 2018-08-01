8c851ed45928e1941929f450166d6e2af01c4310
Brenden Plong <Karunaplong@yahoo.com>
2018-08-01 14:21:41 -0700

    Modified code
    Added functionality to the script which now detects player collision and will move the platform based on that.
    
    Note: The platform spazzes out when player is part of the platform, possibly from the holdChar script.

---
+++ CompSciClubSpring2018/Assets/Scripts/Enviornment/HoldCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldCharacter : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        col.transform.parent = gameObject.transform;
+    }
+    void OnTriggerExit(Collider col)
+    {
+        col.transform.parent = null;
+    }
+}
--- CompSciClubSpring2018/Assets/Scripts/Enviornment/MovingPlatformToggle.cs
+++ CompSciClubSpring2018/Assets/Scripts/Enviornment/MovingPlatformToggle.cs
@@ -6,11 +6,13 @@
  * Description: Script will make it so that a platform can be activated when the player collides or interacts with it in some way
  * 
  */
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingPlatformToggle : MonoBehaviour {
+    public GameObject Player;
     public Transform platform; // variable set to change it's position
     public Transform position1; // variable set to change it's position
     public Transform position2; // variable set to change it's position
@@ -18,14 +20,15 @@ public class MovingPlatformToggle : MonoBehaviour {
     public string currentState; // Helps with informing users(Programmer) of the position the platform is transforming to
     public float smooth; // Sets the speed in which the platform will transform to its next position
     public float resetTime; // Sets the time interval in which the platform will move back to its origin
+    
 	// Use this for initialization
 	void Start () {
-        ChangeTarget(); // Calls upon the ChangeTarget() method
+        OnCollisionStay2D();//ChangeTarget();// Calls upon the ChangeTarget() method
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        platform.position = Vector3.Lerp(platform.position, newPosition, smooth * Time.deltaTime); // This allows for the platform to move
+
+    // Update is called once per frame
+    void FixedUpdate () {
+         // This allows for the platform to move
 	}
     void ChangeTarget() // Will monitor the currentState of the platform and update it accordingly 
     {
@@ -46,4 +49,10 @@ public class MovingPlatformToggle : MonoBehaviour {
         }
         Invoke("ChangeTarget", resetTime); // When a position has been reached, this sets the time in which the platform will move again
     }
+    private void OnCollisionStay2D()
+    { 
+            platform.position = Vector3.Lerp(platform.position, newPosition, smooth * Time.deltaTime);
+            ChangeTarget();    
+    }
 }
+
