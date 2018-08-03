e4c67195cc6b0390c3059c48678ceb7f349a21b1
Brenda de los Santos <bdelossantos@my.canyons.edu>
2018-08-02 22:18:47 -0700

    Added more moving platforms
    - Created a regular multi-point moving platform that will relocate to its original position after translating to all positions
    -created two other moving platforms where one can visibly see the path traveled
     *One uses Lerp
     *The other uses MoveTowards
     *Reference video: https://www.youtube.com/watch?v=ZkGPoZOQE5Q
     *I plan to add comments

---
+++ CompSciClubSpring2018/Assets/Scripts/Enviornment/FollowPath.cs
@@ -0,0 +1,63 @@
+﻿/**
+ * Author(s): Brenda De los Santos
+ * Date Created: 8/2/2018
+ * Last Modified: 8/2/2018
+ * File Name: FollowPath.cs
+ * Description: 
+ * **/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour {
+
+	public enum FollowType
+    {
+        MoveTowards,
+        Lerp
+    }
+
+    public FollowType Type = FollowType.MoveTowards;
+
+    public PathDefinition Path;
+
+    public float speed = 1;
+
+    public float MaxDistanceToGoal = 0.1f;
+
+    public IEnumerator<Transform> currentPoint;
+
+    public void Start()
+    {
+        if (Path == null)
+        {
+            Debug.LogError("Path cannot be null", gameObject);
+            return;
+        }
+
+        currentPoint = Path.GetPathEnumerator();
+        currentPoint.MoveNext();
+
+        if (currentPoint.Current == null)
+            return;
+
+        transform.position = currentPoint.Current.position;
+
+    }
+
+
+    public void Update()
+    {
+        if (currentPoint == null || currentPoint.Current == null)
+            return;
+
+        if (Type == FollowType.MoveTowards)
+            transform.position = Vector3.MoveTowards(transform.position, currentPoint.Current.position, speed * Time.deltaTime);
+        else if (Type == FollowType.Lerp)
+            transform.position = Vector3.Lerp(transform.position, currentPoint.Current.position, speed * Time.deltaTime);
+
+        var distanceSquared = (transform.position - currentPoint.Current.position).sqrMagnitude;
+        if (distanceSquared < MaxDistanceToGoal * MaxDistanceToGoal)
+            currentPoint.MoveNext();
+    }
+}
---
+++ CompSciClubSpring2018/Assets/Scripts/Enviornment/MutliPointPlatform.cs
@@ -0,0 +1,79 @@
+﻿/**
+ * Author(s): Brenda De los Santos
+ * Date Created: 7/27/2018
+ * Last Modified: 7/27/2018
+ * File Name: MultiPointPlatform.cs
+ * Description: This program provides the functionslity of a moving platform that travels to multiple points.
+ **/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MutliPointPlatform : MonoBehaviour {
+
+    [SerializeField]
+    public float speed;
+    
+    private Vector3 startPos; //Holds the start position of the platform
+    private int index;//The index of the positions array
+
+    private Vector3 nextPos;//Holds the next position of the platform
+
+    [SerializeField]
+    private Transform platform; //Reference to the actual platform
+    [SerializeField]
+    private Transform[] transformPos; //Reference to all positions
+
+
+    // Use this for initialization
+    void Start () {
+        
+        //initializes the start and next position
+        startPos = platform.localPosition;
+        nextPos = transformPos[index].localPosition;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Move();
+        
+        //Checks if the platform is nesr the next position to change direction
+        if (Vector3.Distance(platform.localPosition, nextPos) <= 0.1 ) 
+        {
+            if (index == transformPos.Length) //Can be modified to make the platform move back and forth
+            {
+                index = 0;
+                StartPosition(); 
+                nextPos = transformPos[index].localPosition;
+            }
+
+            ChangeDestination(index);
+            
+            index++;
+        }
+    }
+
+    //Moves position of platform from current position to the next
+    private void Move()
+    {
+        platform.localPosition = Vector3.MoveTowards(platform.localPosition, nextPos, speed * Time.deltaTime);
+
+    }
+
+    //Changes the destination of the platform
+    private void ChangeDestination(int index)
+    {
+        nextPos = transformPos[index].localPosition;
+
+    }
+    
+    //Repositions the platform to the orginal position when finished
+    private void StartPosition()
+    {
+        platform.localPosition = startPos;
+    }
+}
