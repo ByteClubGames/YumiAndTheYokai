c0e9a3c3c7c07c1d7c1763fd3fb823df3166d46e
Brenda de los Santos <bdelossantos@my.canyons.edu>
2018-08-03 12:05:45 -0700

    Updated comments
    - Input comments on the FollowPath.cs and PathDefinition.cs scripts

--- CompSciClubSpring2018/Assets/Scripts/Enviornment/FollowPath.cs
+++ CompSciClubSpring2018/Assets/Scripts/Enviornment/FollowPath.cs
@@ -1,10 +1,13 @@
 ﻿/**
  * Author(s): Brenda De los Santos
  * Date Created: 8/2/2018
- * Last Modified: 8/2/2018
+ * Last Modified: 8/3/2018
  * File Name: FollowPath.cs
- * Description: 
- * **/
+ * Description: This programs controls the motion of the platform. It allows the user
+ * to select a follow type of MoveTowards(default) and Lerp. 
+ * This program takes influence from the creators of 3DBuzz's video "Creating 2D 
+ * Games in Unity 4.5 #4 - Moving Platforms"
+ **/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,18 +20,24 @@ public class FollowPath : MonoBehaviour {
         Lerp
     }
 
+    //Sets default FollowType to MoveTowards
     public FollowType Type = FollowType.MoveTowards;
 
+    //Reference to a path the platform needs to follow
     public PathDefinition Path;
 
     public float speed = 1;
 
+    //The maximum distance to a point in the path that is allowed until it move to 
+    //the next point in the path
     public float MaxDistanceToGoal = 0.1f;
 
-    public IEnumerator<Transform> currentPoint;
+    //Creates a path enumerator, allows you to retrieve the current point in the path in the right order
+    private IEnumerator<Transform> currentPoint;
 
     public void Start()
     {
+        //Alerts the user that a path is not being referenced
         if (Path == null)
         {
             Debug.LogError("Path cannot be null", gameObject);
@@ -38,9 +47,11 @@ public class FollowPath : MonoBehaviour {
         currentPoint = Path.GetPathEnumerator();
         currentPoint.MoveNext();
 
+        //Breaks out of start method because there are no points in the path
         if (currentPoint.Current == null)
             return;
-
+        
+        //Sets the position of the object to the first point in the path
         transform.position = currentPoint.Current.position;
 
     }
@@ -48,14 +59,17 @@ public class FollowPath : MonoBehaviour {
 
     public void Update()
     {
+        //Breaks out of update if the object was created without a path or was created with a path that didn't atleast have one point
         if (currentPoint == null || currentPoint.Current == null)
             return;
 
+        //Moves the platform according to the FollowType selected
         if (Type == FollowType.MoveTowards)
             transform.position = Vector3.MoveTowards(transform.position, currentPoint.Current.position, speed * Time.deltaTime);
         else if (Type == FollowType.Lerp)
             transform.position = Vector3.Lerp(transform.position, currentPoint.Current.position, speed * Time.deltaTime);
 
+        //Determines whether the platform is close enough to the target point to begin moving to the next point
         var distanceSquared = (transform.position - currentPoint.Current.position).sqrMagnitude;
         if (distanceSquared < MaxDistanceToGoal * MaxDistanceToGoal)
             currentPoint.MoveNext();
--- CompSciClubSpring2018/Assets/Scripts/Enviornment/PathDefinition.cs
+++ CompSciClubSpring2018/Assets/Scripts/Enviornment/PathDefinition.cs
@@ -1,11 +1,14 @@
 ﻿/**
  * Author(s): Brenda De los Santos
  * Date Created: 8/2/18
- * Last Modified: 8/2/18
+ * Last Modified: 8/3/18
  * File Name: PathDefinition.cs
- * Description: 
+ * Description: This program hold an array of Transforms/points that we want the 
+ * platform to traverse.
+ * This program takes influence from the creators of 3DBuzz's video "Creating 2D 
+ * Games in Unity 4.5 #4 - Moving Platforms"
  **/
- 
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,13 +19,18 @@ public class PathDefinition : MonoBehaviour {
     [SerializeField]
     public Transform[] points;
 
+    //Custom enumerator that will keep the platform moving from point to point (ping-pong motion)
     public IEnumerator<Transform> GetPathEnumerator()
     {
+        //Will terminate the sequence immediately if there is nothing in the points array
+        //Need at least two points to create a path
         if (points == null || points.Length < 1)
             yield break;
 
-        var direction = 1;
+        var direction = 1; 
         var index = 0;
+
+        //Iterator block, used to change direction 
         while (true)
         {
             yield return points[index];
@@ -32,6 +40,7 @@ public class PathDefinition : MonoBehaviour {
 
             if (index <= 0)
                 direction = 1;
+
             else if (index >= points.Length - 1)
                 direction = -1;
 
@@ -39,11 +48,14 @@ public class PathDefinition : MonoBehaviour {
         }
     }
 
+    //Draws the path between points in the scene view of unity
     public void OnDrawGizmos()
     {
+        //Checks if ther are atleast two points in the path
         if (points == null || points.Length < 2)
             return;
 
+        //Draws the line fron the previous point to the current point
         for (var i = 1; i < points.Length; i++)
         {
             Gizmos.DrawLine(points[i - 1].position, points[i].position);
