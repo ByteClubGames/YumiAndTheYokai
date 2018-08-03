d0c477c842b17cbe1428b7fbc6a54443932c7bd3
Unknown <theredpok@yahoo.com>
2018-08-02 17:56:22 -0700

    Initial Movement Revision Push
    Sideways movement and collision functions have been added. The strafeVector has been added to the Move() function.

--- CompSciClubSpring2018/Assets/Scripts/Player/CharacterDirector/PlayerController.cs
+++ CompSciClubSpring2018/Assets/Scripts/Player/CharacterDirector/PlayerController.cs
@@ -27,10 +27,13 @@ public class PlayerController : MonoBehaviour {
     public bool isGrounded;
     private float currGravity;
 
+    private float currSideMovement;
+    public float maxRayDistance = 2.1f; // used in side collision; makes sure cross rays don't go above and below character.
+
     private Vector3 jumpVector;
     private Vector3 strafeVector;
     private float jumpForce;
-    private float velocity;
+    public float velocity = 2;
 
     void Start()
     {
@@ -56,7 +59,10 @@ public class PlayerController : MonoBehaviour {
     public void Move()
     {
         Gravity(false);
+        MoveRight(false);
+        MoveLeft(false);
         jumpVector = new Vector3(0f, currGravity * Time.deltaTime, 0f);
+        strafeVector = new Vector3(currSideMovement * Time.deltaTime, 0f, 0f);  // currSideMovement is changed in the MoveRight() and MoveLeft() functions
         playerTransform.position += (jumpVector);
     }
 
@@ -109,6 +115,88 @@ public class PlayerController : MonoBehaviour {
         else
         {
             isGrounded = false;
+            return Vector3.zero;
+        }
+    }
+
+
+    public void MoveRight(bool isMovingRight)   //Same methods from the Inputlistener script
+    {
+        currSideMovement = 0f;
+
+        if (isMovingRight)
+        {
+            currSideMovement = velocity; //currSideMovement will be added to the strafeVector in the Move() function to determine sideways movement
+        }
+    }
+
+    public void MoveLeft(bool isMovingLeft)
+    {
+        currSideMovement = 0f;
+
+        if (isMovingLeft && currSideMovement <= 0)  // You cant be moving left and right at the same time
+        {
+            currSideMovement = -velocity;
+        }
+    }
+
+    public Vector3 SideCollision() //Incomplete function
+    {
+        // Crossing hit detection rays similar to 'ground checker' from above
+        Ray diagonalDownRight = new Ray(uRC, new Vector3(.1F,-2.2F,0)); // top right corner to the bottom right; Values are dependant on the character size
+        RaycastHit hitDiagonalDownRight;
+
+        Ray diagonalUpRight = new Ray(bRC, new Vector3(.1F, 2.2F, 0)); //  bottom right corner to the top right
+        RaycastHit hitDiagonalUpRight;
+
+        Ray diagonalDownLeft = new Ray(uLC, new Vector3(-.1F, 2.2F, 0)); //  top left corner to the bottom left
+        RaycastHit hitDiagonalDownLeft;
+
+        Ray diagonalUpLeft = new Ray(bLC, new Vector3(-.1F, -2.2F, 0)); //  bottom left corner to the top left
+        RaycastHit hitDiagonalUpLeft;
+
+        Ray upRight = new Ray(uLC, Vector3.right); //from left to right
+        RaycastHit hitUpRight;
+
+        Ray downRight = new Ray(bLC, Vector3.right); //from left to right
+        RaycastHit hitDownRight;
+
+        Ray upLeft = new Ray(uRC, Vector3.left); //from right to left
+        RaycastHit hitUpLeft;
+
+        Ray downLeft = new Ray(uLC, Vector3.left); //from right to left
+        RaycastHit hitDownLeft;
+
+        if (Physics.Raycast(diagonalDownRight, out hitDiagonalDownRight, maxRayDistance, ground) ||
+            Physics.Raycast(diagonalUpRight, out hitDiagonalUpRight, maxRayDistance, ground))
+        {
+            Debug.Log("Player is touching a wall");
+            return hitDiagonalDownRight.point + playerTransform.position;
+        }
+
+        if (Physics.Raycast(diagonalDownLeft, out hitDiagonalDownLeft, maxRayDistance, ground) ||
+            Physics.Raycast(diagonalUpLeft, out hitDiagonalUpLeft, maxRayDistance, ground))
+        {
+            Debug.Log("Player is touching a wall");
+            return hitDiagonalDownRight.point + playerTransform.position;
+        }
+
+        if (Physics.Raycast(upRight, out hitUpRight, .5f, ground) ||
+            Physics.Raycast(downRight, out hitDownRight, 5f, ground))
+        {
+            Debug.Log("Player is touching a wall");
+            return hitDiagonalDownRight.point + playerTransform.position;
+        }
+
+        if (Physics.Raycast(upLeft, out hitUpLeft, .5f, ground) ||
+            Physics.Raycast(downLeft, out hitDownLeft, 5f, ground))
+        {
+            Debug.Log("Player is touching a wall");
+            return hitDiagonalDownRight.point + playerTransform.position;
+        }
+
+        else
+        {
             return Vector3.zero;
         }
     }
