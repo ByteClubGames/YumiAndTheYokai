6204af8c3c73117e57baeef47f88d2e51019225c
Keiran <jamesglynn.k@gmail.com>
2018-09-21 20:28:19 -0700

    Flap Enemy Basically Done
    Ivonne basically finished this enemy, I just added a detection script to detect the player. All it really needs is a better animation, and maybe some code to flip the animation when it changes direction.

--- CompSciClubSpring2018/Assets/Scripts/Enemies/FlyingEnemy.cs
+++ CompSciClubSpring2018/Assets/Scripts/Enemies/FlyingEnemy.cs
@@ -1,26 +1,41 @@
-﻿using System.Collections;
+﻿/*
+ *  Authors: Ivonne Lopez
+ *  Date Created: 9/21/2018
+ *  Last Modified: 9/21/2018
+ *  FlyingEnemy.cs
+ *  Description: Script responsible for flap movement and attack patterns. Calls on PlayerDetection.cs to actually detect the player's presence.
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FlyingEnemy : MonoBehaviour {
 
+    [Header("Horizontal Flight Boundary Points")]
+    public Vector3 pos1 = new Vector3(-4f, 5f, 0f);
+    public Vector3 pos2 = new Vector3(4f, 5f, 0f);
+
+    [Header("Physics/ Enemy Attributes")]
     public float horizontalSpeed;
     public float verticalSpeed;
     public float amplitude;
-    public Vector3 tempPosition;
-    public float progress = 0f;
-    public Transform Player;
     public float attackSpeed = 0.1f;
-    private Vector3 directionOfPlayer;
-    private bool challenged = false;
-    private Vector3 pos1 = new Vector3(-4f, 5f, 0f);
-    private Vector3 pos2 = new Vector3(4f, 5f, 0f);
 
-	// Use this for initialization
-	void Start () {
+    private Transform Player;
+    private Vector3 directionOfPlayer;
+    private Vector3 tempPosition; // This position vector is modified algebraically, and then the enemy's actual position is set equal to this temp position    
+    private float progress = 0f; // Variable determining how the percentage completion of the Lerp for the horizontal component of movement
+    private bool challenged = false; // Determines if the enemy should be attacking the player or not
+    private PlayerDetection detectPlayer;
+
+
+    // Use this for initialization
+    void Awake ()
+    {
         //default position
         tempPosition = transform.position;
-
+        Player = GameObject.Find("Player-Human").transform;
+        detectPlayer = this.GetComponentInChildren<PlayerDetection>();
 	}
 	
 	// Update is called once per frame
@@ -29,28 +44,22 @@ public class FlyingEnemy : MonoBehaviour {
         tempPosition.x = Mathf.Lerp(pos1.x, pos2.x, progress * 1);
         tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
 
-        transform.position = Movement(tempPosition.x, tempPosition.y); 
+        Movement(tempPosition.x, tempPosition.y); 
 	} 
 
-public Vector3 Movement(float x, float y)
+    private void Movement(float x, float y)
     {
-        return new Vector3(x, y, 0f);
-    }
-    void Update()
-    {
-        //gets direction to move towards
+        challenged = detectPlayer.PlayerDetected();
+
         if (challenged)
         {
             directionOfPlayer = Player.transform.position - transform.position;
             directionOfPlayer = directionOfPlayer.normalized;
-            transform.Translate(directionOfPlayer * attackSpeed, Space.World);
+            transform.Translate(directionOfPlayer * attackSpeed, Space.World);            
         }
-    }
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        else
         {
-            challenged = true;
-        }
-    }
+            transform.position = new Vector3(x, y, 0f);
+        }        
+    }   
 }
