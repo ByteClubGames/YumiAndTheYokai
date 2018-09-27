cb691220fb670b72f9f3a752e3f8e32db03f0600
Kross Ruiz <krossruiz@gmail.com>
2018-09-27 14:36:06 -0700

    Made a very pretty scene

---
+++ CompSciClubSpring2018/Assets/Art/bird/BirdSwivel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdSwivel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(Vector3.left, Mathf.Sin(Time.time));
+    }
+}
