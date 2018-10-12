8f771cf5f7bf4bed3ec249df353c482431e810d1
Brenden Plong <Karunaplong@yahoo.com>
2018-10-11 22:25:47 -0700

    Added Scene to play around in

---
+++ CompSciClubSpring2018/Assets/Scripts/OnColDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnColDestroy : MonoBehaviour {
+    public GameObject player;
+    public GameObject enemy;
+	// Use this for initialization
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "ColSphere")
+        {
+            Destroy(enemy);
+        }
+    }
+    // Update is called once per frame
+}
