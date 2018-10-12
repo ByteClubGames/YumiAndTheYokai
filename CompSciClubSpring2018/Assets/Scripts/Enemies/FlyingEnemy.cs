4b3b0082cc6a9416f8cb6ec653562373c9f1b8de
Darrell Wong <theredpok@yahoo.com>
2018-10-12 16:42:57 -0700

    Update to flying enemy
    More adjustable movement

--- CompSciClubSpring2018/Assets/Scripts/Enemies/FlyingEnemy.cs
+++ CompSciClubSpring2018/Assets/Scripts/Enemies/FlyingEnemy.cs
@@ -1,7 +1,7 @@
 ﻿/*
  *  Authors: Ivonne Lopez, Darrell Wong
  *  Date Created: 9/21/2018
- *  Last Modified: 10/11/2018
+ *  Last Modified: 10/12/2018
  *  FlyingEnemy.cs
  *  Description: Script responsible for general(flap and grounded) movement and attack patterns. Calls on PlayerDetection.cs to actually detect the player's presence.
  */
@@ -9,7 +9,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlyingEnemy : MonoBehaviour {
+public class FlyingEnemy : MonoBehaviour
+{
 
     [Header("Horizontal Flight Boundary Points")]
     public Vector3 pos1;  //pos1 and pos2 are the initial positions of the enemy
@@ -17,57 +18,64 @@ public class FlyingEnemy : MonoBehaviour {
     public float leftBoundry = 4f;   //how far the enemy will wander to the left when not under aggro
     public float rightBoundry = 4f;  // how far the enemy will wanted to the right when not under aggro
     public Vector3 startPos;
+    private Vector3 difference;     //how far away the 2 boundry points are. Used in calculating horizontal speeds
 
 
     [Header("Physics/ Enemy Attributes")]
-    public float horizontalSpeed;
-    public float verticalSpeed;
-    public float amplitude;
-    public float attackSpeed = 0.05f;
-    public float returnSpeed = 0.05f;
+    public float patrolSpeed = 0.1f;        //patrolSpeed needs to be between 0 and .5f. An increase of patrol Speed requires an increase of normalspeed
+    public float verticalSpeed;             //?
+    public float amplitude;                 //amplitude of the enemy patrol pattern
+    public float attackSpeed = 5f;          //speed of enemy when attacking player
+    public float normalSpeed = 2f;          //speed of the enemy when not attacking
 
     private Transform Player;
     private Vector3 directionOfPlayer;
-    public Vector3 tempPosition; // This position vector is modified algebraically, and then the enemy's actual position is set equal to this temp position    
-    private float progress = 0f; // Variable determining how the percentage completion of the Lerp for the horizontal component of movement
-    private bool challenged = false; // Determines if the enemy should be attacking the player or not
+    public Vector3 tempPosition;            // This position vector is modified algebraically, and then the enemy's actual position is set equal to this temp position    
+    private float progress = 0f;            // Variable determining how the percentage completion of the Lerp for the horizontal component of movement
+    private bool challenged = false;        // Determines if the enemy should be attacking the player or not
     private PlayerDetection detectPlayer;
     Rigidbody rb;
 
     // Use this for initialization
-    void Awake ()
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
         startPos = transform.position;
         tempPosition = transform.position;
 
-        pos1 = new Vector3(transform.position.x, transform.position.y, 0f);
+        pos1 = new Vector3(transform.position.x - leftBoundry, transform.position.y, 0f);
         pos2 = new Vector3(transform.position.x + rightBoundry, transform.position.y, 0f);
 
         Player = GameObject.Find("Yumi").transform;
         detectPlayer = this.GetComponentInChildren<PlayerDetection>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    }
 
-        progress = Mathf.PingPong(Time.time * 0.1f, 1f);
-        tempPosition.x = Mathf.Lerp(pos1.x, pos2.x, progress * 1);
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        pos1 = new Vector3(startPos.x - leftBoundry, startPos.y, 0f);
+        pos2 = new Vector3(startPos.x + rightBoundry, startPos.y, 0f);
+        difference = pos2 - pos1;
+
+        progress = Mathf.PingPong(Time.time * patrolSpeed, 1f);
+
+        Debug.DrawLine(pos1, pos2, Color.blue);
+
+        tempPosition.x = Mathf.Lerp(pos1.x, pos2.x, progress);
         tempPosition.y = startPos.y + (Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude); ;
 
-        Movement(tempPosition); 
-	} 
+        Movement(tempPosition);
+    }
 
     private void Movement(Vector3 nextPosition)     //nextPosition is the current position(before being challenged)
     {
         challenged = detectPlayer.PlayerDetected();
-        
+
         if (challenged)
         {
             directionOfPlayer = (Player.transform.position - transform.position).normalized;
-            //rb.AddForce(directionOfPlayer*attackSpeed);
             rb.MovePosition(transform.position + directionOfPlayer * attackSpeed * Time.deltaTime);
-            //transform.Translate(directionOfPlayer * attackSpeed, Space.World);            
         }
         else
         {
@@ -78,8 +86,8 @@ public class FlyingEnemy : MonoBehaviour {
             }
             else
             {
-                rb.MovePosition(transform.position + directionOfNextPosition * returnSpeed * Time.deltaTime);
+                rb.MovePosition(transform.position + directionOfNextPosition * normalSpeed * Time.deltaTime);
             }
-        }        
-    }   
+        }
+    }
 }
