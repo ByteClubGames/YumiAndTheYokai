0bd07a3df732dd6a514223de6ef9e4a249350306
Brenden Plong <Karunaplong@yahoo.com>
2018-10-19 16:54:51 -0700

    Added a Scene and a Destruction Script
    Gym to play in and a script to make things dissapear

--- CompSciClubSpring2018/Assets/Scripts/OnColDestroy.cs
+++ CompSciClubSpring2018/Assets/Scripts/OnColDestroy.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
+﻿/*
+*
+*Creator(s).........................................................Brenden Plong
+*Created..............................................................10/05/2018
+*Last Modified............................................@ 4:30PM on 10/19/2018
+*Last Modified by...................................................Brenden Plong
+*
+*Description:   Modular script that is used to destroy objecs based on collisions
+**
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class OnColDestroy : MonoBehaviour {
-    public GameObject player;
-    public GameObject enemy;
-	// Use this for initialization
-
+    public GameObject player; // Used for grabbing the "player" object
+    public GameObject enemy; // Used for grabbing the "enemy" object
+    public LayerMask foe;
+    public LayerMask friend;
+    //(other.gameObject.layer == 9) 
     public void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             Destroy(enemy);
+            Debug.Log("Has Collided");
         }
-    }
-    // Update is called once per frame
+    } 
 }
