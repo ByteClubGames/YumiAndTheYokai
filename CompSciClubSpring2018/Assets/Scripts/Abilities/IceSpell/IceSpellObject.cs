71fb2c8bd1acfa2eb06a8936606a976bccdc3842
porcupal <michael.d.sanchez@gmail.com>
2018-12-17 18:41:00 -0800

    Ice Spell changes WIP

---
+++ CompSciClubSpring2018/Assets/Scripts/Abilities/IceSpell/IceSpellObject.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class IceProjectile : MonoBehaviour
+{
+    public GameObject iceSpellPrefab;
+    private Plane referencePlane = new Plane(new Vector3(0, 0, -1), new Vector3(0, 0, 0));
+    Rigidbody spellObjectRB;
+    private bool isActive;
+
+    void Start()
+    {
+        print("object created");
+        spellObjectRB = GetComponent<Rigidbody>();
+        //transform.position = GameObject.Find("SpellCaster").transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("collision detected!");
+        if (!collision.collider.CompareTag("player"))
+        {
+            // reset spellObject location to spellCaster position
+            transform.position = GameObject.Find("SpellCaster").transform.position;
+            Instantiate(iceSpellPrefab, GetSpellClickTarget(), Quaternion.identity);
+            Destroy(gameObject, 3f); // end spell life-cycle
+        }
+    }
+
+    public Vector3 GetSpellClickTarget()
+    {
+        Vector3 mousePosFar = new Vector3(Input.mousePosition.x,
+                                          Input.mousePosition.y,
+                                          Camera.main.farClipPlane);
+        Vector3 mousePosNear = new Vector3(Input.mousePosition.x,
+                                           Input.mousePosition.y,
+                                           Camera.main.nearClipPlane);
+        Vector3 mousePosF = Camera.main.ScreenToWorldPoint(mousePosFar);
+        Vector3 mousePosN = Camera.main.ScreenToWorldPoint(mousePosNear);
+
+        // shoot a ray from nearClipPlane -> farClipPlane 
+        Ray screenPoint = new Ray(mousePosN, mousePosF - mousePosN);
+
+        // get worldCursorPosition/vector3 of spell click target on reference plane
+        float distanceCamToPlane = 0f;
+        referencePlane.Raycast(screenPoint, out distanceCamToPlane);
+        Vector3 worldCursorPosition = screenPoint.GetPoint(distanceCamToPlane);
+
+        // final spell path direction = projected vector from (mousePosF - mousePosN) onto reference plane 
+        worldCursorPosition = Vector3.ProjectOnPlane(worldCursorPosition, new Vector3(0, 0, 0));
+
+        // get SpellCaster current position
+        Vector3 castPos = GameObject.Find("SpellCaster").transform.position;
+
+        // unity debug rays. blue: yumi -> spell click point, red: yumi -> (mousePosF - mousePosN) 
+        UnityEngine.Debug.DrawRay(castPos, worldCursorPosition - castPos, Color.blue);
+        UnityEngine.Debug.DrawRay(mousePosN, (mousePosF - mousePosN) * 100, Color.red);
+
+        // fix z-coordinate
+        Vector3 direction = worldCursorPosition - castPos;
+        direction.z = 0;
+
+        // final spellClickTarget is (world position - yumi's position)
+        return direction;
+    }
+
+    void Update()
+    {
+        //if (isActive)
+        //{
+        //    print("should be moving now");
+        //    spellObjectRB.MovePosition(transform.position + GetSpellClickTarget().normalized * 7 * Mathf.Sin(Time.deltaTime));
+        //}
+        //if (Vector3.Distance(transform.position, GetSpellClickTarget()) < 0.1f)
+        //{
+        //    isActive = false;
+        //}
+    }
+}
--- CompSciClubSpring2018/Assets/Scripts/Abilities/IceSpell/IceSpellUse.cs
+++ CompSciClubSpring2018/Assets/Scripts/Abilities/IceSpell/IceSpellUse.cs
@@ -1,11 +1,16 @@
-
-﻿/* IceSpellUse.cs
- * Date Created: 3/10/18
- * Last Edited: 9/13/18
- * Programmer: Jack Bruce, Brenden Plong
- * Description: Script for instantiating ice spell objects upon mouse clicks
- * Update @ 9/13/2018: Changed previous method of casting the spell to ray casting to allow for more "accurate" spawning.
- */
+/* 
+********************************************************************************
+*Creator(s)............................................Jack Bruce, Brenden Plong 
+*Created..............................................................03/10/2018 
+*Last Modified............................................@ 5:00PM on 12/14/2018 
+*Last Modified by................................................Michael Sanchez 
+* 
+*Description:   This script is used to instantiating ice spell objects upon 
+*               mouse clicks. Update @ 9/13/2018: Changed previous method of 
+*               casting the spell objects to ray casting to allow for more 
+*               "accurate" spawning. 
+********************************************************************************
+*/
 
 using System.Collections;
 using System.Collections.Generic;
@@ -14,83 +19,109 @@ using UnityEngine;
 
 public class IceSpellUse : MonoBehaviour
 {
+    //public GameObject iceSpellPrefab;
+    //private IceProjectile iceProjectile;
+    public GameObject IceProjectile;
+    private Rigidbody IceProjectileRB;
+    private Plane referencePlane = new Plane(new Vector3(0, 0, -1), new Vector3(0, 0, 0));
 
-    public GameObject iceSpellPrefab;
-    private GameObject myCurrentObject;
-    private bool _isDragging = false;
-    public Transform camera;
-    public float zComponent = Camera.main.transform.position.z;
+    //private GameObject spellObject = GameObject.Find("SpellObject"); // spell projectile
+    //private GameObject castObject = GameObject.Find("SpellCaster"); // spell source
+
+
+    //private IEnumerator coroutine;
+
 
     void Start()
     {
-        this.GetComponent<TimeManager>().StartSlowDown(); // Time is slowed when spawner is here
+        //coroutine = SpellObjectTransform(spellClickTarget);
+        //StartCoroutine(coroutine);
+        //spellObjectRB = GameObject.Find("SpellObject").GetComponent<Rigidbody>();
+        //StartCoroutine("SpellObjectTransform", spellClickTarget);
+        // initialize spellObject at yumi's current location ??
+        //spellObject.transform.position = castObject.transform.position;
     }
-    
+
+    //private IEnumerator SpellObjectTransform(Vector3 target)
+    //{
+    //    //yield return new WaitUntil(() => isActive);
+    //    //print("coroutine");
+    //    ////while (Vector3.Distance(GameObject.Find("SpellObject").transform.position, target) > 0.05f)
+    //    ////{
+    //    //for(int i=0; i < 25; i++)
+    //    //{
+    //    //    print(i);
+    //    //    float projectileStep = 5;
+    //    //    spellObjectRB.MovePosition(GameObject.Find("SpellObject").transform.position + target.normalized * projectileStep * Mathf.Sin(Time.deltaTime));
+    //    //}
+    //    //}
+    //        //isActive = false;
+    //}
+    public Vector3 GetSpellClickTarget()
+    {
+        Vector3 mousePosFar = new Vector3(Input.mousePosition.x,
+                                          Input.mousePosition.y,
+                                          Camera.main.farClipPlane);
+        Vector3 mousePosNear = new Vector3(Input.mousePosition.x,
+                                           Input.mousePosition.y,
+                                           Camera.main.nearClipPlane);
+        Vector3 mousePosF = Camera.main.ScreenToWorldPoint(mousePosFar);
+        Vector3 mousePosN = Camera.main.ScreenToWorldPoint(mousePosNear);
+
+        // shoot a ray from nearClipPlane -> farClipPlane 
+        Ray screenPoint = new Ray(mousePosN, mousePosF - mousePosN);
+
+        // get worldCursorPosition/vector3 of spell click target on reference plane
+        float distanceCamToPlane = 0f;
+        referencePlane.Raycast(screenPoint, out distanceCamToPlane);
+        Vector3 worldCursorPosition = screenPoint.GetPoint(distanceCamToPlane);
+
+        // final spell path direction = projected vector from (mousePosF - mousePosN) onto reference plane 
+        worldCursorPosition = Vector3.ProjectOnPlane(worldCursorPosition, new Vector3(0, 0, 0));
+
+        // get SpellCaster current position
+        Vector3 castPos = GameObject.Find("SpellCaster").transform.position;
+
+        // unity debug rays. blue: yumi -> spell click point, red: yumi -> (mousePosF - mousePosN) 
+        UnityEngine.Debug.DrawRay(castPos, worldCursorPosition - castPos, Color.blue);
+        UnityEngine.Debug.DrawRay(mousePosN, (mousePosF - mousePosN) * 100, Color.red);
+
+        // fix z-coordinate
+        Vector3 direction = worldCursorPosition - castPos;
+        direction.z = 0;
+
+        // final spellClickTarget is (world position - yumi's position)
+        return direction;
+    }
     void Update()
     {
-        camera = GameObject.Find("Main Camera").transform;
-        //Instantiates iceSpellPrefab upon clicking in the position of the click
-        /*if (Input.GetMouseButtonDown(0))
-        {
-           
-            Vector3 p = Camera.main.ScreenToWorldPoint(new 
-                Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
-            Instantiate(iceSpellPrefab, new Vector3(p.x, p.y, 0.0f), 
-                        Quaternion.identity);
-           iceSpellPrefab.transform.position = Input.mousePosition;
-        }*/
+         
 
         if (Input.GetMouseButtonDown(0))
         {
-            _isDragging = true;
+            // ray from yumiPos to screenPoint
+            //if (Physics.Raycast(yumiRay.origin, fixedWorldCursPos, out spellPath))
+            //{
+            // get the location of the player click (camera through screen point)
+            //Vector3 playerinput = new Vector3(spellPath.point.x, spellPath.point.y, 0);
+
+            print("instantiate");
+            Vector3 spawnLoc = GameObject.Find("SpellCaster").transform.position;
+            spawnLoc.z = 0;
+
+            GameObject clone = Instantiate(IceProjectile, spawnLoc, Quaternion.identity);
+            IceProjectileRB = clone.GetComponent<Rigidbody>();
+
+            Vector3 spellTarget = GetSpellClickTarget();
+            spellTarget.z = 0;
+            IceProjectileRB.velocity = transform.TransformDirection(spellTarget);
+           
+            //GameObject.Find("SpellObject").transform.position = Vector3.MoveTowards(spellPos, spellClickTarget, projectileStep);
+
+            //iceSpellPrefab.transform.position = Input.mousePosition;
+            //}
         }
-        if (Input.GetMouseButtonUp(0))
-        {
-            _isDragging = false;
-            return;
-        }
-        if (_isDragging) // Casts spell only if the mouse is held down and is dragging across the screen
-        {
-            Vector3 p = Camera.main.ScreenToWorldPoint(new
-               Vector3(Input.mousePosition.x, Input.mousePosition.y, zComponent));
-            Instantiate(iceSpellPrefab, new Vector3(p.x, p.y, 0.0f),
-                        Quaternion.identity);
-            iceSpellPrefab.transform.position = Input.mousePosition;
-            //Runs ice prefab is mouse button is pushed down
-        
-        }
+        //StopCoroutine("SpellObjectTransform");
+    } // end Update(): void
 
-        /* 
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit))
-        {
-            Vector3 playerinput = new Vector3(hit.point.x, hit.point.y, 0);
-            Instantiate(iceSpellPrefab, new Vector3(p.x, p.y, 0.0f),
-                        Quaternion.identity);
-            iceSpellPrefab.transform.position = Input.mousePosition;
-
-        }
-        /*
-
-
-
-
-    /*function Update()
-    var objectToInstantiate : GameObject;
- private var myCurrentObject : GameObject;
- 
- function Update(){
- if(Input.GetMouseButtonDown(0)){
- myCurrentObject = Instantiate(objectToInstantiate,Input.mousePosition,Quaternion.identity);
-     }
- if(Input.GetMouseButton(0) && myCurrentObject){
- myCurrentObject.transform.position = Input.mousePosition;
-     }
- if(Input.GetMouseButtonUp(0) && myCurrentObject){
- myCurrentObject = null;
-     }
- 
-    }*/
-        }
-}
+} // end class IceSpellUse : Monobehaviour
