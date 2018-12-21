e442853911c3e0718c6c1439c326fdae1f0c4291
Keiran <jamesglynn.k@gmail.com>
2018-12-21 11:19:35 -0800

    Merged '111-IceSpell Into Master
    Merged your branch into master, excluding the project.txt file. In the future, we should look into adjusting the animation as it gives the appearance that the spell is being fired twice (snowflakes appear then flicker in a wave making it look like two beams were shot).
    
    Overall really great work and the code is super clean.

--- CompSciClubSpring2018/Assets/Scripts/Abilities/IceSpell/IceSpellUse.cs
+++ CompSciClubSpring2018/Assets/Scripts/Abilities/IceSpell/IceSpellUse.cs
@@ -1,11 +1,16 @@
-
-﻿/* IceSpellUse.cs
- * Date Created: 3/10/18
- * Last Edited: 9/13/18
- * Programmer: Jack Bruce, Brenden Plong
- * Description: Script for instantiating ice spell objects upon mouse clicks
- * Update @ 9/13/2018: Changed previous method of casting the spell to ray casting to allow for more "accurate" spawning.
- */
+/* 
+********************************************************************************
+*Creator(s)............................................Jack Bruce, Brenden Plong 
+*Created..............................................................03/10/2018 
+*Last Modified............................................@ 5:00PM on 12/14/2018 
+*Last Modified by................................................Michael Sanchez 
+* 
+*Description:   This script is used to instantiating ice spell objects upon 
+*               mouse clicks. Update @ 9/13/2018: Changed previous method of 
+*               casting the spell objects to ray casting to allow for more 
+*               "accurate" spawning. 
+********************************************************************************
+*/
 
 using System.Collections;
 using System.Collections.Generic;
@@ -14,83 +19,17 @@ using UnityEngine;
 
 public class IceSpellUse : MonoBehaviour
 {
+    public GameObject IceProjectile;
 
-    public GameObject iceSpellPrefab;
-    private GameObject myCurrentObject;
-    private bool _isDragging = false;
-    public Transform camera;
-    public float zComponent = Camera.main.transform.position.z;
-
-    void Start()
-    {
-        this.GetComponent<TimeManager>().StartSlowDown(); // Time is slowed when spawner is here
-    }
-    
     void Update()
     {
-        camera = GameObject.Find("Main Camera").transform;
-        //Instantiates iceSpellPrefab upon clicking in the position of the click
-        /*if (Input.GetMouseButtonDown(0))
-        {
-           
-            Vector3 p = Camera.main.ScreenToWorldPoint(new 
-                Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
-            Instantiate(iceSpellPrefab, new Vector3(p.x, p.y, 0.0f), 
-                        Quaternion.identity);
-           iceSpellPrefab.transform.position = Input.mousePosition;
-        }*/
-
         if (Input.GetMouseButtonDown(0))
         {
-            _isDragging = true;
+            print("instantiate");
+            Vector3 spawnLoc = GameObject.Find("SpellCaster").transform.position;
+
+            GameObject clone = Instantiate(IceProjectile, spawnLoc, Quaternion.identity) as GameObject;
         }
-        if (Input.GetMouseButtonUp(0))
-        {
-            _isDragging = false;
-            return;
-        }
-        if (_isDragging) // Casts spell only if the mouse is held down and is dragging across the screen
-        {
-            Vector3 p = Camera.main.ScreenToWorldPoint(new
-               Vector3(Input.mousePosition.x, Input.mousePosition.y, zComponent));
-            Instantiate(iceSpellPrefab, new Vector3(p.x, p.y, 0.0f),
-                        Quaternion.identity);
-            iceSpellPrefab.transform.position = Input.mousePosition;
-            //Runs ice prefab is mouse button is pushed down
-        
-        }
+    }
 
-        /* 
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit))
-        {
-            Vector3 playerinput = new Vector3(hit.point.x, hit.point.y, 0);
-            Instantiate(iceSpellPrefab, new Vector3(p.x, p.y, 0.0f),
-                        Quaternion.identity);
-            iceSpellPrefab.transform.position = Input.mousePosition;
-
-        }
-        /*
-
-
-
-
-    /*function Update()
-    var objectToInstantiate : GameObject;
- private var myCurrentObject : GameObject;
- 
- function Update(){
- if(Input.GetMouseButtonDown(0)){
- myCurrentObject = Instantiate(objectToInstantiate,Input.mousePosition,Quaternion.identity);
-     }
- if(Input.GetMouseButton(0) && myCurrentObject){
- myCurrentObject.transform.position = Input.mousePosition;
-     }
- if(Input.GetMouseButtonUp(0) && myCurrentObject){
- myCurrentObject = null;
-     }
- 
-    }*/
-        }
 }
