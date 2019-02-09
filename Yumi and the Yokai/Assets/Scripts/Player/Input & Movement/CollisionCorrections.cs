812b8cfd65eae5a66194ca3da39a0d4595da85ec
Keiran <jamesglynn.k@gmail.com>
2019-02-09 14:07:32 -0800

    Progress Push

--- Yumi and the Yokai/Assets/Scripts/Player/Input & Movement/CollisionCorrections.cs
+++ Yumi and the Yokai/Assets/Scripts/Player/Input & Movement/CollisionCorrections.cs
@@ -26,20 +26,32 @@ public class CollisionCorrections : MonoBehaviour {
     private Vector3 BL; // Bottom Left corner of the box collider
     private Vector3 BR; // Bottom Right corner of the box collider
 
+    private LayerMask platformMask;
+
     private int numberVerticalRays;
     private int numberHorizontalRays;
-
-    private LayerMask platformMask;
 
     private float skinWidth;
     private float headCheck;
     private float maxClimbableSlope;
     private float error;
+    private float transformHeight;
+    private float transformWidth;
+
+    //Hit detection flags
+    private bool isClimbableSlope;
+    private bool isGrounded;
+    private bool isOnSlope;
+    private bool isRight;
+    private bool isOnWallRight;
+    private bool isOnWallLeft;
+
+    
 
     /// <summary>
     /// Constructor for the Collision Corrections class. This object holds data about the player's current position and the dimensions
     /// of the player's box collider. It includes methods for checking for collisions in the direction of movement, and adjusting the
-    /// distance moved such to avoid clipping.
+    /// distance moved in order to avoid clipping.
     /// </summary>
     /// <param name="player_transform">The transform of the player.</param>
     /// <param name="box_collider">The box collider attached to the player.</param>
@@ -49,7 +61,8 @@ public class CollisionCorrections : MonoBehaviour {
     /// <param name="skin_width">How long do the rays need to be when checking for collisions.</param>
     /// <param name="head_check">Length of a ray used for checking for low hanging roofs.</param>
     /// <param name="max_climbable_slope">How steep of a slope is too steep to walk on.</param>
-    /// <param name="collision_check_error">How much leeway to use when checking for collisions</param>
+    /// <param name="collision_check_error">How much leeway to use when checking for collisions. Having at
+    /// least a small value here (less than 0.001f) can help with performance of collision checking.</param>
     public CollisionCorrections(Transform player_transform, BoxCollider box_collider, LayerMask platform_mask, 
         int number_of_vertical_rays, int number_of_horizontal_rays, float skin_width, float head_check,
         float max_climbable_slope, float collision_check_error)
@@ -63,6 +76,14 @@ public class CollisionCorrections : MonoBehaviour {
         skinWidth = skin_width;
         headCheck = head_check;
         error = collision_check_error;
+
+        //Initialize all hit detection flags to false
+        isClimbableSlope = false;
+        isGrounded = false;
+        isOnSlope = false;
+        isRight = false;
+        isOnWallRight = false;
+        isOnWallLeft = false;
 
         //Initialize the positions for corners of the box collider
         RaycastStartPoints();
@@ -83,11 +104,30 @@ public class CollisionCorrections : MonoBehaviour {
         BR = new Vector3(skinBounds.max.x, skinBounds.min.y, 0f);
     }
     
+    /// <summary>
+    /// Calculates the height and width of the transform
+    /// </summary>
+    private void TransformDimentions()
+    {
+        transformHeight = transform.localScale.y;
+        transformWidth = transform.localScale.x;
+    }
 
+    /// <summary>
+    /// Clear hit detection flags for wall sliding right and left
+    /// </summary>
+    private void ClearOnWall()
+    {
+        isOnWallLeft = isOnWallRight = false;
+    }
 
-    public static Vector3 HorizontalCollision(PlayerController playerController,Vector3 deltaMovement, BoxCollider boxCollider, Vector3 BL, Vector3 BR, 
-        float transformHeight, float verticalRaySeparation, float maxClimbableSlope, float skinWidth, float error, int horizontalRays, 
-        bool climbableSlope, bool isRight, bool isGrounded, LayerMask platformMask)
+    /// <summary>
+    /// Check for collisions on the left/right axis. If a collision occurs, adjust the movement that was planned for this frame, such that 
+    /// the player does not clip through a platform.
+    /// </summary>
+    /// <param name="deltaMovement">The ammount & direction of movement for this frame</param>
+    /// <returns></returns>
+    public Vector3 HorizontalCollision(Vector3 deltaMovement)
     {
         isRight = deltaMovement.x > 0f;
         float rayLength = Mathf.Abs(deltaMovement.x) + skinWidth;
@@ -95,11 +135,11 @@ public class CollisionCorrections : MonoBehaviour {
         Vector3 firstRayStartPoint = isRight ? BR : BL;
 
         float colliderUseableHeight = boxCollider.size.y * Mathf.Abs(transformHeight) - (2f * skinWidth);
-        verticalRaySeparation = colliderUseableHeight / (horizontalRays - 1);
+        float verticalRaySeparation = colliderUseableHeight / (numberHorizontalRays - 1);
 
-        for (int i = 0; i < horizontalRays; i++)
+        for (int i = 0; i < numberHorizontalRays; i++)
         {
-            climbableSlope = false;
+            isClimbableSlope = false;
             Vector3 ray = new Vector3(firstRayStartPoint.x, firstRayStartPoint.y + i * verticalRaySeparation, 0f);
             RaycastHit hit;
 
@@ -114,11 +154,11 @@ public class CollisionCorrections : MonoBehaviour {
 
                 if (i > 0) //case of a collision with a narrowing ceiling while walking
                 {
-                    climbableSlope = false;
+                    isClimbableSlope = false;
                 }
                 else
                 {
-                    climbableSlope = true;
+                    isClimbableSlope = true;
                 }
             }
 
@@ -128,38 +168,38 @@ public class CollisionCorrections : MonoBehaviour {
 
                 if (i > 0) //case of a collision with a narrowing ceiling while walking
                 {
-                    climbableSlope = false;
+                    isClimbableSlope = false;
                 }
                 else
                 {
-                    climbableSlope = true;
+                    isClimbableSlope = true;
                 }
             }
 
-            if (raycastHit && !climbableSlope)
+            if (raycastHit && !isClimbableSlope)
             {
                 // something something Darrell plz make slopes work ... ok
 
                 deltaMovement.x = hit.point.x - ray.x;
                 rayLength = Mathf.Abs(deltaMovement.x);
 
-                if (isRight && !climbableSlope)
+                if (isRight && !isClimbableSlope)
                 {
                     deltaMovement.x -= skinWidth;
 
                     if ((hit.transform.gameObject.tag == "WallJump") && !isGrounded)
                     {
-                        playerController.SetIsOnWallRight(true);
+                        //////////playerController.SetIsOnWallRight(true);
                         Debug.Log("isOnWallRight");
                     }
                 }
-                else if (!isRight && !climbableSlope)
+                else if (!isRight && !isClimbableSlope)
                 {
                     deltaMovement.x += skinWidth;
 
                     if ((hit.transform.gameObject.tag == "WallJump") && !isGrounded)
                     {
-                        playerController.SetIsOnWallLeft(true);
+                        //////////playerController.SetIsOnWallLeft(true);
                         Debug.Log("isOnWallLeft");
                     }
                 }
@@ -171,14 +211,18 @@ public class CollisionCorrections : MonoBehaviour {
             }
         }
 
-        playerController.SetClimbableSlope(climbableSlope);
-        playerController.SetIsRight(isRight);
+        //playerController.SetClimbableSlope(climbableSlope); .......... Not important to return to movement script
+        //playerController.SetIsRight(isRight); .......... Not important to return to movement script
         return deltaMovement;
     }
 
-    public static Vector3 VerticalCollision(PlayerController playerController, Vector3 deltaMovement, BoxCollider boxCollider, Vector3 TL, Vector3 BL,
-        float transformWidth, float horizontalRaySeparation, float skinWidth, float error, int verticalRays, bool isOnSlope, bool isRight, 
-        bool isGrounded, LayerMask platformMask)
+    /// <summary>
+    /// Check for collisions on the up/down axis. If a collision occurs, adjust the movement that was planned for this frame, such that 
+    /// the player does not clip through a platform.
+    /// </summary>
+    /// <param name="deltaMovement">The ammount & direction of movement for this frame</param>
+    /// <returns></returns>
+    public Vector3 VerticalCollision(Vector3 deltaMovement)
     {
         bool isUp = deltaMovement.y > 0f;
         isRight = deltaMovement.x > 0f;
@@ -189,12 +233,12 @@ public class CollisionCorrections : MonoBehaviour {
 
 
         float colliderUseableWidth = boxCollider.size.x * Mathf.Abs(transformWidth) - (2f * skinWidth);
-        horizontalRaySeparation = colliderUseableWidth / (verticalRays - 1);
+        float horizontalRaySeparation = colliderUseableWidth / (numberVerticalRays - 1);
         if (isRight)
         {
             //print("moving Right");
 
-            for (int i = verticalRays; i >= 0; i--)
+            for (int i = numberVerticalRays; i >= 0; i--)
             {
                 Vector3 ray = new Vector3(firstRayStartPoint.x + i * horizontalRaySeparation, firstRayStartPoint.y, 0f);
                 RaycastHit hit;
@@ -216,7 +260,7 @@ public class CollisionCorrections : MonoBehaviour {
                     {
                         deltaMovement.y += skinWidth;
                         isGrounded = true;
-                        playerController.ClearOnWall();
+                        //////////playerController.ClearOnWall();
                     }
 
                     if (!isUp && deltaMovement.y > 0.00001f)
@@ -243,7 +287,7 @@ public class CollisionCorrections : MonoBehaviour {
         {
             //print("moving left");
 
-            for (int i = 0; i < verticalRays; i++)
+            for (int i = 0; i < numberVerticalRays; i++)
             {
                 Vector3 ray = new Vector3(firstRayStartPoint.x + i * horizontalRaySeparation, firstRayStartPoint.y, 0f);
                 RaycastHit hit;
@@ -265,7 +309,7 @@ public class CollisionCorrections : MonoBehaviour {
                     {
                         deltaMovement.y += skinWidth;
                         isGrounded = true;
-                        playerController.ClearOnWall();
+                        //////////playerController.ClearOnWall();
                     }
 
                     if (!isUp && deltaMovement.y > 0.00001f)
@@ -289,14 +333,13 @@ public class CollisionCorrections : MonoBehaviour {
             }
         }
 
-        playerController.SetIsGrounded(isGrounded);
-        playerController.SetIsOnSlope(isOnSlope);
-        playerController.SetIsRight(isRight);
+        //////////playerController.SetIsGrounded(isGrounded);
+        //////////playerController.SetIsOnSlope(isOnSlope);
+        //////////playerController.SetIsRight(isRight);
         return deltaMovement;
     }
 
-    public static Vector3 VerticalSlopeDetection(PlayerController playerController, Vector3 deltaMovement, BoxCollider boxCollider, Vector3 BL, Vector3 BR,
-        float skinWidth,float maxClimbableSlope, LayerMask platformMask)
+    public Vector3 VerticalSlopeDetection(Vector3 deltaMovement)
     {
         // slope check from the center of our collider
         float centerOfCollider = (BL.x + BR.x) * 0.5f;
@@ -329,6 +372,40 @@ public class CollisionCorrections : MonoBehaviour {
         }
         return deltaMovement;
     }
+
+    //Methods that give info about current state of collision. To be accessed by other scripts.
+    #region Public Accessor Methods for Hit Detection Flags
+    public bool getIsClimbableSlope()
+    {
+        return isClimbableSlope;
+    }
+
+    public bool getIsGrounded()
+    {
+        return isGrounded;
+    }
+
+    public bool getIsOnSlope()
+    {
+        return isOnSlope;
+    }
+
+    public bool getIsRight()
+    {
+        return isRight;
+    }
+
+    public bool getIsOnWallRight()
+    {
+        return isOnWallRight;
+    }
+
+    public bool getIsOnWallLeft()
+    {
+        return isOnWallLeft;
+    }
+    #endregion
+
     #region LegacyCode
     //bool handleHorizontalSlope(ref Vector3 deltaMovement, float angle)
     //{
