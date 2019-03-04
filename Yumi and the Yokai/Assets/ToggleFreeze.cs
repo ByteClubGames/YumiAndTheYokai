813861d5ad572da400dc79a6aaae307b175ee09f
Keiran <jamesglynn.k@gmail.com>
2019-03-03 20:07:22 -0800

    Added Comments
    I removed two redundant variables, added some comments, and attached the yumi to the main follow camera in the ice thaw scene.

--- Yumi and the Yokai/Assets/ToggleFreeze.cs
+++ Yumi and the Yokai/Assets/ToggleFreeze.cs
@@ -6,7 +6,7 @@
 *Last Modified by................................................Brenden Plong
 *
 *Description:   Handles the freezing of water so that the player can walk on 
-*               it
+*               it.
 ********************************************************************************
 */
 using System.Collections;
@@ -15,55 +15,77 @@ using UnityEngine;
 
 public class ToggleFreeze : MonoBehaviour
 {
-    public GameObject sphere;
     public Material ice;
     public Material water;
     public BoxCollider waterBlockCollider;
-    int countDown = 5;
-    int i = 0;
-    timer iceTimer = new timer();
-    // Start is called before the first frame update
+    public int countDown = 5; // Ammount of time (in seconds) before the ice thaws.
+    Timer iceTimer = new Timer();
+
     void Start()
     {
         waterBlockCollider = this.GetComponent<BoxCollider>();
-        timer iceTimer = new timer(0, 0f);
-        waterBlockCollider.enabled = false;
+        Timer iceTimer = new Timer(0, 0f); // Initializing a "Null" timer.
+        waterBlockCollider.enabled = false; // Starting the water in a "thawed" state.
     }
-    // Update is called once per frame
+    
+    /* Every frame, if there is an active timer, check if the required time has passed so that you can unfreeze the 
+     * ice block. When the timer is up, we change the material back to a water material, create a null timer, and then
+     * disable the collider on the water block so the player can not walk on it. */
     void FixedUpdate()
     {
-        if (iceTimer.waitTime != 0)
+        if (iceTimer.waitTime != 0) // Is there an active, valid, non-NULL timer?
         {
             if (iceTimer.checkTimer(Time.time))
             {
                 this.GetComponent<Renderer>().material = water;
-                iceTimer = new timer(0, 0f); // Resets the timer for turning the ice back to water
+                iceTimer = new Timer(0, 0f); // Resets the timer for turning the ice back to water
                 waterBlockCollider.enabled = false; // Disables the box collider at the start
             }
         }
     }
+
+    /* When this function is entered, we check if the trigger that entered the collider was an ice spell projectile. If
+     * it was, we change the material of the water to an ice material, enable the block's collider so it can be walked
+     * on (freeze the water), and start a new timer that, when finished, will thaw the ice. */
     void OnTriggerEnter(Collider hitInfo)
     {
         IceProjectile iceBall = hitInfo.GetComponent<IceProjectile>();
-        if (iceBall != null)//turns the water into ice
-        {
-            Debug.Log("*Freezing*");
-            this.GetComponent<Renderer>().material = ice; // Changes the renderer of the block
-            waterBlockCollider.enabled = true; // Makes the box collider active
-            iceTimer = new timer(countDown, Time.time);
+        if (iceBall != null)
+        { 
+        Debug.Log("*Freezing*");
+        this.GetComponent<Renderer>().material = ice;
+        waterBlockCollider.enabled = true;
+        iceTimer = new Timer(countDown, Time.time);
         }
     }
-    struct timer
+
+    /// <summary>
+    /// This struct is essentially a timer object.
+    /// </summary>
+    struct Timer
     {
         float endTime;
         public int waitTime;
         public float curTime;
-        public timer(int wait_Time, float cur_Time)
+
+        /// <summary>
+        /// This is the constructor for the Timer struct. It takes in the current time and the time you want it to wait for. It
+        /// contains a method for checking if the specified wait time has elapsed or not.
+        /// </summary>
+        /// <param name="wait_Time">The time, in seconds, you would like the time to wait for.</param>
+        /// <param name="cur_Time">This is the current time. You should probably pass in Time.time.</param>
+        public Timer(int wait_Time, float cur_Time)
         {
             waitTime = wait_Time;
             curTime = cur_Time;
             endTime = waitTime + cur_Time;
         }
+
+        /// <summary>
+        /// Method that will return true if the the specified time to the timer has been passed.
+        /// </summary>
+        /// <param name="curTime">This is the current time. You should probably pass in Time.time.</param>
+        /// <returns></returns>
         public bool checkTimer(float curTime)
         {
             if(curTime > endTime)
