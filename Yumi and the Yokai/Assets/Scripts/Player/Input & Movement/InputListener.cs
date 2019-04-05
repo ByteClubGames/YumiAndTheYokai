5518eab68cc905300a13b6ee485a33820944445f
Keiran <jamesglynn.k@gmail.com>
2019-04-05 16:09:14 -0700

    Adjustments to InputLIstener.cs
    making a few changes to this class so that it is cleaner and will interface well with a new movement system. I also want to add some error handling to it so that the game doesn't completely break if some of the variables it is expecting are undefined.

--- Yumi and the Yokai/Assets/Scripts/Player/Input & Movement/InputListener.cs
+++ Yumi and the Yokai/Assets/Scripts/Player/Input & Movement/InputListener.cs
@@ -1,131 +1,167 @@
-﻿///*
-// * Programmer: Keiran Glynn, Daniel Jaffe, Spencer Wilson, Michael Sanchez
-// * Date Created: 07/23/2018 @ 12:30 AM
-// * Last Modified: 10/20/2018 @ 12:34 AM
-// * Last Modification By: Daniel Jaffe
-// * File Name: InputListener.cs
-// * Description: This class is responsible listening to the keyboard, and calling the corresponding actions (movement, spells, etc). 
-// */
+﻿/*
+ * Programmer: Keiran Glynn, Daniel Jaffe, Spencer Wilson, Michael Sanchez
+ * Date Created: 07/23/2018 @ 12:30 AM
+ * Last Modified: 10/20/2018 @ 12:34 AM
+ * Last Modification By: Daniel Jaffe
+ * File Name: InputListener.cs
+ * Description: This class is responsible listening to the keyboard, and calling the corresponding actions (movement, spells, etc). 
+ */
 
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class InputListener : MonoBehaviour
-//{
-//    private PlayerController human;
-//    private PlayerController yokai;
-//    private PlayerController activePlayer; // Specifies which game object the movement is being called on (Yumi or Yokai)
-//    private YokaiSwitcher switcher; // Script responsible for spawning and deleting the Yokai
-//    private SpellCasting spellcaster;
-//    private VisibilityController invisibleObjects;
-//    private bool yumiActive; // Flag for if the human is currently active
+public class InputListener : MonoBehaviour
+{
+    Dictionary<string, string> ControlList;
 
+    void Start()
+    {
+        Dictionary<string, string> ControlList = new Dictionary<string, string>();
+    }
 
-//    void Start()
-//    {
-//        human = GameObject.Find("Yumi").GetComponent<PlayerController>();
-//        switcher = GameObject.Find("Yumi").GetComponentInChildren<YokaiSwitcher>();
-//        spellcaster = GameObject.Find("Yumi").GetComponentInChildren<SpellCasting>(); //You will need a gameObject attached to Yumi called SpellAbilities. On that object, attach the script SpellCasting and the spell spawner objects to that script.
-//        invisibleObjects = GameObject.Find("SceneDirector").GetComponentInChildren<VisibilityController>();
+    
 
-//        switcher.SetSpawnOffset(true); // If Yokai is spawned, do so on the right side of human by default
-//        activePlayer = human;
-//        yumiActive = true;
-//    }
+    void Update()
+    {
+        if (CheckForInput())
+        {
+            JumpInput();
+            CancelJumpInput();
+            LeftInput();
+            RightInput();
+            YokaiInput();
+            SpellInput();
+        }        
+    }
 
-//    void Update()
-//    {
-//        activePlayer = yumiActive ? human : yokai; // Choose which character to call movement methods on
-//                                                   //Debug.Log(activePlayer + " is now active");
+    /// <summary>
+    /// Define a keyboard-key for each action by assigning the keyboard-key as a value in this dictionary.
+    /// </summary>
+    private void AssignControls()
+    {        
+        ControlList.Add("jump1", "w");
+        ControlList.Add("jump2", "up");
+        ControlList.Add("jump3", "space");
+        ControlList.Add("left1", "a");
+        ControlList.Add("left2", "left");
+        ControlList.Add("right1", "d");
+        ControlList.Add("right2", "right");
+        ControlList.Add("yokai", "f");
+        ControlList.Add("earth", "1");
+        ControlList.Add("ice", "2");
+        ControlList.Add("wind", "3");
+        ControlList.Add("cancel spells", "escape");
+        
+        //ControlList.Add("action", "key");
+    }
 
-//        if ((Input.GetKey("a") || Input.GetKey("left")) && (Input.GetKey("d") || Input.GetKey("right")))
-//        {
-//            activePlayer.CallLeft(false);
-//            activePlayer.CallRight(false);
-//        }
-//        else if (Input.GetKey("a") || Input.GetKey("left"))
-//        {
-//            activePlayer.CallLeft(true);
-//            activePlayer.CallRight(false);
-//            switcher.SetSpawnOffset(false); // If Yokai is spawned, do so on the left side of human
-//        }
-//        else if (Input.GetKey("d") || Input.GetKey("right"))
-//        {
-//            activePlayer.CallLeft(false);
-//            activePlayer.CallRight(true);
-//            switcher.SetSpawnOffset(true); // If Yokai is spawned, do so on the right side of human
-//        }
-//        else
-//        {
-//            activePlayer.CallLeft(false);
-//            activePlayer.CallRight(false);
-//        }
+    /// <summary>
+    /// Method to edit the keyboard-key associated with a given action.
+    /// </summary>
+    /// <param name="action">The action that you want to re-assign.</param>
+    /// <param name="inputKey">They keyboard-key that you want to assign the action to.</param>
+    public void EditControls(string action, string inputKey)
+    {
+        ControlList[action] = inputKey;
+    }
 
-//        // Jumping
-//        if (Input.GetKeyDown("w") || Input.GetKeyDown("up") || Input.GetKeyDown("space"))
-//        {
-//            activePlayer.CallJump();
-//        }
+    /// <summary>
+    /// Checks to see if any buttons have been pressed this frame, or if a button has been held from a previous frame.
+    /// </summary>
+    /// <returns></returns>
+    private bool CheckForInput()
+    {
+        return (Input.anyKey && Input.anyKeyDown);
+    }
 
-//        if (Input.GetKeyUp("w") || Input.GetKeyUp("up") || Input.GetKeyUp("space"))
-//        {
-//            activePlayer.CallShortHop();
-//        }
+    private void JumpInput()
+    {
+        if (Input.GetKeyDown(ControlList["jump1"]))
+        {
 
-//        // Character Swap
-//        if (Input.GetKeyDown("f")) //Updated the mapping to "f" to be in line with most other games. 1-3 will be for spells. -Daniel Jaffe
-//        {
-//            if (GameObject.Find("Yokai(Clone)") == null)
-//            {
-//                switcher.SpawnYokai();
-//                invisibleObjects.SetVisible();
-//                yokai = GameObject.Find("Yokai(Clone)").GetComponent<PlayerController>();                
-//                activePlayer.ClearCalls();
-//                SetYumiActive(false);                
-//            }
-//            else
-//            {
-//                switcher.DeleteYokai(GameObject.Find("Yokai(Clone)"));
-//                invisibleObjects.SetInvisible();
-//                SetYumiActive(true);
-//            }
-//        }
+        }
+        else if (Input.GetKeyDown(ControlList["jump2"]))
+        {
 
-//        // Spell Casting!!! (Finally-- lol)
-//        if (Input.GetKeyDown("1") && yumiActive) //Press 1 and start the earth spell
-//        {
-//            if (spellcaster.GetSpellStatus())
-//            {
-//                spellcaster.DestroySpellSpawner();
-//            }
-//            spellcaster.CallEarthSpell();
-//        }
-//        if (Input.GetKeyDown("2") && yumiActive) //Press 2 and start the ice spell
-//        {
-//            if (spellcaster.GetSpellStatus())
-//            {
-//                spellcaster.DestroySpellSpawner();
-//            }
-//            spellcaster.CallIceSpell();
-//        }
-//        if (Input.GetKeyDown("3") && yumiActive) //Press 3 and start the wind spell
-//        {
-//            if (spellcaster.GetSpellStatus())
-//            {
-//                spellcaster.DestroySpellSpawner();
-//            }
-//            spellcaster.CallWindSpell();
-//        }
-//        if (Input.GetKeyDown("escape") || (!yumiActive && GameObject.Find("Yokai(Clone)") != null)) //Press escape and cancel out of spell casting mode. Also cancel if Yokai is spawned.
-//        {
-//            spellcaster.DestroySpellSpawner();
-//        }
+        }
+        else if (Input.GetKeyDown(ControlList["jump3"]))
+        {
 
-//    }
+        }
+    }
 
-//    public void SetYumiActive(bool active)
-//    {
-//        yumiActive = active ? true : false;
-//    }
-//}
+    private void CancelJumpInput()
+    {
+        if (Input.GetKeyUp(ControlList["jump1"]))
+        {
+
+        }
+        else if (Input.GetKeyUp(ControlList["jump2"]))
+        {
+
+        }
+        else if (Input.GetKeyUp(ControlList["jump3"]))
+        {
+
+        }
+    }
+
+    private void LeftInput()
+    {
+        if (Input.GetKey(ControlList["left1"]))
+        {
+
+        }
+        else if (Input.GetKey(ControlList["left2"]))
+        {
+
+        }
+    }
+
+    private void RightInput()
+    {
+        if (Input.GetKey(ControlList["right1"]))
+        {
+
+        }
+        else if (Input.GetKey(ControlList["right2"]))
+        {
+
+        }
+    }
+
+    private void YokaiInput()
+    {
+        if (Input.GetKey(ControlList["yokai"]))
+        {
+
+        }
+    }
+
+    private void SpellInput()
+    {
+        if (Input.GetKeyDown(ControlList["earth"]))
+        {
+
+        }
+        else if (Input.GetKeyDown(ControlList["ice"]))
+        {
+
+        }
+        else if (Input.GetKeyDown(ControlList["wind"]))
+        {
+
+        }
+
+        if (Input.mouseScrollDelta.y != 0f)
+        {
+            //SwapActiveSpell();
+        }
+
+        if (Input.GetKeyDown(ControlList["cancel spells"]))
+        {
+
+        }
+    }
+}
