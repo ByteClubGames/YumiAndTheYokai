99674f0db7e8660feaee41cc0205679be874c33c
Unknown <Lopezivonne2940@yahoo.com>
2019-04-06 14:26:15 -0700

    Finished Fixing the Visibility Controller
    This script will now disable the mesh renderer of any object that is assigned to the visibility controller

--- Yumi and the Yokai/Assets/Scripts/Enviornment/VisibilityController.cs
+++ Yumi and the Yokai/Assets/Scripts/Enviornment/VisibilityController.cs
@@ -1,4 +1,12 @@
-﻿using System.Collections;
+﻿/* 
+ * Authors: Ivonne Lopez
+ * Date Created:  04/06/2019 @  2:00 PM
+ * Date Modified: 04/06/2019 @  3:00 PM
+ * Project: YumiAndTheYokai
+ * Description: This script enables/disables the renderer of any object passed to the visibility controller. 
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,24 +16,25 @@ public class VisibilityController : MonoBehaviour
 
     public void SetVisible()
     {
-        
+        //Handles UnassignedReferenceException
         try
         {
-            InvisibleObject.SetActive(true);
+            InvisibleObject.GetComponent<Renderer>().enabled = true;
         }
-        catch (System.NullReferenceException)
+        catch (UnassignedReferenceException)
         {
-            Debug.LogError(InvisibleObject);
+            Debug.Log("Missing Object");
         }
     }
     public void SetInvisible()
     {
         try {
-            InvisibleObject.SetActive(false);
+            //disables renderer 
+            InvisibleObject.GetComponent<Renderer>().enabled = false;
         }
-        catch (System.NullReferenceException)
+        catch (UnassignedReferenceException)
         {
-            Debug.LogError(InvisibleObject);
+            Debug.Log("Missing Object");
         }
     }
 }
