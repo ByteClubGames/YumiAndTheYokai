f89aac8466f0eca1d33f0c7ac0e4b32e324a3a1b
Daniel Jaffe <flarros@gmail.com>
2019-05-03 16:22:04 -0700

    Minor tweaks
    Now uses array of structs holding a vector and their hit

--- Yumi and the Yokai/Assets/Scripts/Abilities/EarthSpellScripts/EarthSpellUse.cs
+++ Yumi and the Yokai/Assets/Scripts/Abilities/EarthSpellScripts/EarthSpellUse.cs
@@ -1,18 +1,8 @@
-﻿/* EarthSpellUse.cs
- * Date Created: 3/15/18
- * Last Edited: 12/16/18
- * Programmer: Daniel Jaffe & Darrell Wong
- * Description: Spawn in the earthSpell object - Attach to the Earth Spell Spawner object:
- *      1. Uses a OverlapSphere to check eSpell overlap
- *      2. Spawns a eSpell object at the point of click
- *      
- */
-
-/*
+﻿/*
 * ************************************************************************************************
 * Creator(s)...........................................................Daniel Jaffe & Darrell Wong
 * Created...............................................................................03/15/2018
-* Last Modified............................................................@ 11:33PM on 12/21/2018
+* Last Modified............................................................@ 11:33PM on 04/19/2018
 * Last Modified by....................................................................Daniel Jaffe
 * 
 * 
@@ -43,33 +33,66 @@ public class EarthSpellUse : MonoBehaviour
         //execute on mouse click
         if (Input.GetMouseButtonDown(0))
         {
-            //raycast from the camera and the player - if both rays hit the same object with the tag EarthSpellSurface, then spawn eSpell
-            //***Should eventually be moved into Spell Casting script as this is used with the Ice Spell as well and a similar version for wind***
-            #region Yumi and Cam Ray Setup
-            //Ray from camera to world position
-            Vector3 mousePosFar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane);
-            Vector3 mousePosNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
-            Vector3 mousePosF = Camera.main.ScreenToWorldPoint(mousePosFar);
-            Vector3 mousePosN = Camera.main.ScreenToWorldPoint(mousePosNear);
-            Ray camRay = new Ray(mousePosN, mousePosF - mousePosN); //this is the ray shot from the camera to to world position clicked at
-            Debug.DrawRay(mousePosN, mousePosF-mousePosN, Color.green); //this is a visual representation of that camera ray
-            RaycastHit camHit; //our camRay hit variable
+            ClickLocation();
+            CheckOverlap();
 
-            //Ray from Yumi to the location the camera ray hits when z=0
-            float distanceCamToPlane = 0f;
-            zPlane.Raycast(camRay, out distanceCamToPlane);
-            Vector3 worldCursorPosition = camRay.GetPoint(distanceCamToPlane);
-            Vector3 yumiPosition = GameObject.Find("SpellCaster").transform.position;
-            worldCursorPosition = Vector3.ProjectOnPlane(worldCursorPosition, new Vector3(0, 0, 0));
-            Ray yumiRay = new Ray(yumiPosition, worldCursorPosition - yumiPosition); //this is the ray shot from yumi to the world position
-            Debug.DrawRay(yumiPosition, worldCursorPosition - yumiPosition, Color.blue); //this is a visual representation of that yumi ray
-            RaycastHit yumiHit; //our yumiRay hit variable
-            #endregion
+        }
+    }
 
-            if (Physics.Raycast(camRay, out camHit) && Physics.Raycast(yumiRay, out yumiHit) && (yumiHit.collider.gameObject == camHit.collider.gameObject) && yumiHit.collider.CompareTag("EarthSpellSurface")) 
+    //Struct that holds a Vector and a RaycastHit
+    public struct TargetVector
+    {
+        Ray ray;
+        Raycast hit;
+
+        public TargetVector(Ray ray, Raycast hit)
+        {
+            this.ray = ray;
+            this.hit = hit;
+        }
+    }
+
+    public TargetVector[] ClickLocation()
+    {
+        //raycast from the camera and the player - if both rays hit the same object with the tag EarthSpellSurface, then spawn eSpell
+        //***Should eventually be moved into Spell Casting script as this is used with the Ice Spell as well and a similar version for wind***
+
+        //Ray from camera to world position
+        Vector3 mousePosFar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane);
+        Vector3 mousePosNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+        Vector3 mousePosF = Camera.main.ScreenToWorldPoint(mousePosFar);
+        Vector3 mousePosN = Camera.main.ScreenToWorldPoint(mousePosNear);
+        Ray camRay = new Ray(mousePosN, mousePosF - mousePosN); //this is the ray shot from the camera to to world position clicked at
+        Debug.DrawRay(mousePosN, mousePosF - mousePosN, Color.green); //this is a visual representation of that camera ray for debugging purposes 
+        RaycastHit camHit; //our camRay hit variable
+
+        //Ray from Yumi to the location the camera ray hits when z=0
+        float distanceCamToPlane = 0f;
+        zPlane.Raycast(camRay, out distanceCamToPlane);
+        Vector3 worldCursorPosition = camRay.GetPoint(distanceCamToPlane);
+        Vector3 yumiPosition = GameObject.Find("SpellCaster").transform.position;
+        worldCursorPosition = Vector3.ProjectOnPlane(worldCursorPosition, new Vector3(0, 0, 0)); //FORGET WHAT THIS DOES
+        Ray yumiRay = new Ray(yumiPosition, worldCursorPosition - yumiPosition); //this is the ray shot from yumi to the world position
+        Debug.DrawRay(yumiPosition, worldCursorPosition - yumiPosition, Color.blue); //this is a visual representation of that yumi ray for debugging purposes 
+        RaycastHit yumiHit; //our yumiRay hit variable
+
+        //Creates and returns an array of the struct TargetVector which contains both the vector and its hit.
+        TargetVecotor[] myTarget = new TargetVector[2];
+        myTarget[0] = new TargetVector(camRay, camHit);
+        myTarget[1] = new TargetVector(yumiRay, yumiHit);
+
+        return myTarget;
+    }
+
+    void CheckOverlap()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (Physics.Raycast(camRay, out camHit) && Physics.Raycast(yumiRay, out yumiHit) && (yumiHit.collider.gameObject == camHit.collider.gameObject) && yumiHit.collider.CompareTag("EarthSpellSurface"))
             {
                 //get the location of the player click
-                Vector3 playerinput = new Vector3 (yumiHit.point.x, yumiHit.point.y, 0);
+                Vector3 playerinput = new Vector3(yumiHit.point.x, yumiHit.point.y, 0);
                 normalVector = yumiHit.normal;
                 normalVector.z = 0;
 
@@ -96,6 +119,7 @@ public class EarthSpellUse : MonoBehaviour
     }
 
 
+    //Function to spawn the pilar in and scale it correctly
     void ScaleAndSpawn(RaycastHit yumiHit)
     {
         int layerMask = 1 << 9; //turns the layermask on for the player layer
